d09f987229f081b1e14804f44a7e6dfa50f502c2
shashidharpc <shashidharpc10@gmail.com>
2026-06-01 22:43:53 +0530

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = window.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = "https://task-manager-zabj.onrender.com";
 
 function getToken() {
   return localStorage.getItem("token");
